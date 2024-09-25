dc65b2df449ee5e8dfb2a0f0b5359fdebbc8233a
Danyil <dartomov@gmail.com>
2024-09-25 11:53:38 +0200

    Create paths for create, read, update, delete views for Tag and Task models

---
+++ todo_list/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+
+from todo_list.views import (
+    TaskListView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView
+)
+
+app_name = "todo"
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+]
